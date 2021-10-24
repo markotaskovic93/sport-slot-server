3f0d535539d6806a538af72081f885dba3457aaf
Marko Taskovic <marko.taskovic@fazi.rs>
2021-10-24 22:01:08 +0200

    set local and prod port

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import sequelize from './drivers/db/database'
 const app = express()
 
 app.get('/', (req, res) => {
-    //res.send(sequelize)
     console.log('what is DB: ', sequelize)
 })
 
-app.listen(4500, () => {
+app.listen(process.env.PORT || 4500, () => {
     console.log('app is running on port 4500')
 })
 
