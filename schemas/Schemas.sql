8ebae009c865113afd20c31049eac05a18d3cdc0
Marko Tasković <marko.taskovic@ad.badin.rs>
2022-04-06 12:04:25 +0200

    updated schemas for DB

--- schemas/Schemas.sql
+++ schemas/Schemas.sql
@@ -1,31 +1,20 @@
-CREATE TABLE IF NOT EXISTS public."Players"
+CREATE TABLE IF NOT EXISTS public."Court_owner_payment_methods"
 (
     id bigint NOT NULL,
-    full_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    birthday character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    height character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    state character varying(255) COLLATE pg_catalog."default",
-    city character varying(255) COLLATE pg_catalog."default",
-    street character varying(255) COLLATE pg_catalog."default",
-    gender character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    age character varying(255) COLLATE pg_catalog."default",
-    email character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    phone character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    password character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    bio text COLLATE pg_catalog."default",
-    sport character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    email_verified boolean NOT NULL,
-    phone_verified boolean NOT NULL,
-    terms_conditions boolean NOT NULL,
+    court_owner_id bigint NOT NULL,
+    first_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    card_number character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    expiration_month character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    expiration_year character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    cvv character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    zip_code character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    state character varying(255) COLLATE pg_catalog."default" NOT NULL,
     blocked boolean NOT NULL,
-    notification_invites boolean NOT NULL,
-    notification_messages boolean NOT NULL,
-    notification_reminders boolean NOT NULL,
-    notification_promotions boolean NOT NULL,
     "createdAt" timestamp with time zone NOT NULL,
     "updatedAt" timestamp with time zone NOT NULL,
-    CONSTRAINT "Players_pkey" PRIMARY KEY (id),
-    CONSTRAINT "Players_email_key" UNIQUE (email)
+    CONSTRAINT "Court_owner_payment_methods_pkey" PRIMARY KEY (id),
+    CONSTRAINT "Court_owner_payment_methods_card_number_key" UNIQUE (card_number)
 );
 
 CREATE TABLE IF NOT EXISTS public."Court_owners"
@@ -33,10 +22,8 @@ CREATE TABLE IF NOT EXISTS public."Court_owners"
     id bigint NOT NULL,
     first_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
     last_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    birthday character varying(255) COLLATE pg_catalog."default",
     state character varying(255) COLLATE pg_catalog."default",
     city character varying(255) COLLATE pg_catalog."default",
-    street character varying(255) COLLATE pg_catalog."default",
     email character varying(255) COLLATE pg_catalog."default" NOT NULL,
     password character varying(255) COLLATE pg_catalog."default" NOT NULL,
     phone character varying(255) COLLATE pg_catalog."default" NOT NULL,
@@ -73,36 +60,14 @@ CREATE TABLE IF NOT EXISTS public."Courts"
     CONSTRAINT "Courts_pkey" PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS public."Court_owner_payment_methods"
+CREATE TABLE IF NOT EXISTS public."Fees"
 (
     id bigint NOT NULL,
-    court_owner_id bigint NOT NULL,
-    first_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    last_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    card_number character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    expiration_month character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    expiration_year character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    cvv character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    zip_code character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    state character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    blocked boolean NOT NULL,
+    slot_base_fee character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    slot_additional_fee character varying(255) COLLATE pg_catalog."default" NOT NULL,
     "createdAt" timestamp with time zone NOT NULL,
     "updatedAt" timestamp with time zone NOT NULL,
-    CONSTRAINT "Court_owner_payment_methods_pkey" PRIMARY KEY (id),
-    CONSTRAINT "Court_owner_payment_methods_card_number_key" UNIQUE (card_number)
-);
-
-CREATE TABLE IF NOT EXISTS public."Booked_slots"
-(
-    id bigint,
-    court_id bigint,
-    slot_id bigint,
-    player_id bigint,
-    player_needed character varying(255) COLLATE pg_catalog."default",
-    reservation_status character varying(255) COLLATE pg_catalog."default",
-    blocked character varying(255) COLLATE pg_catalog."default",
-    "createdAt" timestamp with time zone NOT NULL,
-    "updatedAt" timestamp with time zone NOT NULL
+    CONSTRAINT "Fees_pkey" PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS public."Player_payment_methods"
@@ -124,6 +89,109 @@ CREATE TABLE IF NOT EXISTS public."Player_payment_methods"
     CONSTRAINT "Player_payment_methods_card_number_key" UNIQUE (card_number)
 );
 
+CREATE TABLE IF NOT EXISTS public."Players"
+(
+    id bigint NOT NULL,
+    full_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    birthday character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    height character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    state character varying(255) COLLATE pg_catalog."default",
+    city character varying(255) COLLATE pg_catalog."default",
+    street character varying(255) COLLATE pg_catalog."default",
+    gender character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    age character varying(255) COLLATE pg_catalog."default",
+    email character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    phone character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    bio text COLLATE pg_catalog."default",
+    prefered_sport character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    balance integer NOT NULL,
+    email_verified boolean NOT NULL,
+    phone_verified boolean NOT NULL,
+    terms_conditions boolean NOT NULL,
+    blocked boolean NOT NULL,
+    notification_invites boolean NOT NULL,
+    notification_messages boolean NOT NULL,
+    notification_reminders boolean NOT NULL,
+    notification_promotions boolean NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    CONSTRAINT "Players_pkey" PRIMARY KEY (id),
+    CONSTRAINT "Players_email_key" UNIQUE (email)
+);
+
+CREATE TABLE IF NOT EXISTS public."Players_notifications"
+(
+    id bigint NOT NULL,
+    slot_id bigint NOT NULL,
+    reservation_id bigint NOT NULL,
+    player_id bigint NOT NULL,
+    notification_type character varying(255) COLLATE pg_catalog."default",
+    notification_desc character varying(255) COLLATE pg_catalog."default",
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    CONSTRAINT "Players_notifications_pkey" PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public."Players_transactions_histories"
+(
+    id bigint NOT NULL,
+    player_id bigint,
+    transaction_type character varying(255) COLLATE pg_catalog."default",
+    transaction_desc character varying(255) COLLATE pg_catalog."default",
+    transaction_time character varying(255) COLLATE pg_catalog."default",
+    transaction_amount character varying(255) COLLATE pg_catalog."default",
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    CONSTRAINT "Players_transactions_histories_pkey" PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public."Reservation_players"
+(
+    id bigint NOT NULL,
+    reservation_id bigint,
+    player_id bigint,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    CONSTRAINT "Reservation_players_pkey" PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public."Reservation_players_invitations"
+(
+    id bigint NOT NULL,
+    reservation_id bigint,
+    player_id bigint,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    CONSTRAINT "Reservation_players_invitations_pkey" PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public."Reservation_players_requests"
+(
+    id bigint NOT NULL,
+    reservation_id bigint,
+    player_id bigint,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    CONSTRAINT "Reservation_players_requests_pkey" PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS public."Reservations"
+(
+    id bigint NOT NULL,
+    slot_id bigint NOT NULL,
+    admin_player_id character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    players_needed character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    players_accepted character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    sport character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    reservation_type character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    price_per_person character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    is_active boolean NOT NULL,
+    "createdAt" timestamp with time zone NOT NULL,
+    "updatedAt" timestamp with time zone NOT NULL,
+    CONSTRAINT "Reservations_pkey" PRIMARY KEY (id)
+);
+
 CREATE TABLE IF NOT EXISTS public."Slots"
 (
     id bigint NOT NULL,
@@ -132,10 +200,17 @@ CREATE TABLE IF NOT EXISTS public."Slots"
     slot_start_time character varying(255) COLLATE pg_catalog."default" NOT NULL,
     slot_end_time character varying(255) COLLATE pg_catalog."default" NOT NULL,
     slot_price character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    slot_base_price character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    slot_discounted_price character varying(255) COLLATE pg_catalog."default",
     slot_discount character varying(255) COLLATE pg_catalog."default",
-    slot_paid boolean NOT NULL,
+    slot_city character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    slot_state character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    slot_has_reservation boolean,
+    slot_reservation_id character varying(255) COLLATE pg_catalog."default",
+    slot_available_sports character varying(255)[] COLLATE pg_catalog."default" NOT NULL,
     slot_booked boolean NOT NULL,
     slot_blocked boolean NOT NULL,
+    slot_active boolean NOT NULL,
     "createdAt" timestamp with time zone NOT NULL,
     "updatedAt" timestamp with time zone NOT NULL,
     CONSTRAINT "Slots_pkey" PRIMARY KEY (id)
